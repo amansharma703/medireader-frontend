f64f1a9a19c078bacca0718ee0a275a17e017651
amansharma703 <aman703jk@gmail.com>
2024-09-17 14:30:23 +0530

    Initial commit

---
+++ src/app/api/create-chat/route.ts
@@ -0,0 +1,46 @@
+import { db } from "@/lib/db";
+import { chats } from "@/lib/db/schema";
+import { loadS3IntoPinecone } from "@/lib/pinecone";
+import { getS3Url } from "@/lib/s3";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+// /api/create-chat
+export async function POST(req: Request, res: Response) {
+  console.log("_____________123_______________");
+
+  const { userId } = await auth();
+  if (!userId) {
+    return NextResponse.json({ error: "unauthorized" }, { status: 401 });
+  }
+  try {
+    const body = await req.json();
+    const { file_key, file_name } = body;
+    console.log(file_key, file_name);
+    await loadS3IntoPinecone(file_key);
+    const chat_id = await db
+      .insert(chats)
+      .values({
+        fileKey: file_key,
+        pdfName: file_name,
+        pdfUrl: getS3Url(file_key),
+        userId,
+      })
+      .returning({
+        insertedId: chats.id,
+      });
+
+    return NextResponse.json(
+      {
+        chat_id: chat_id[0].insertedId,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("🔥internal server Error 1", error);
+    return NextResponse.json(
+      { error: "🔥internal server Error 2" },
+      { status: 500 }
+    );
+  }
+}
